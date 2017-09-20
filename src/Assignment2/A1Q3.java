aef7c7fee05a8ceaa09e2667b4806c88f67b2bc1
modia9548 <modia9548@HRH-IC0027333.wrdsb.ca>
2017-09-20 13:01:03 -0400

    I have completed up to question 3 in assignment 2

---
+++ src/Assignment2/A1Q3.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment2;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/**
+ *
+ * @author Aakash Modi
+ */
+public class A1Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create the city and robot
+        City aakash = new City();
+        Robot modi = new Robot(aakash, 57, 26, Direction.WEST);
+        //make sure the robot is facing south
+        while(modi.getDirection()!=Direction.NORTH){
+            modi.turnLeft();
+        }
+       //get the robot to keep moving until it reaches street 0 
+        while(modi.getStreet() != 0){
+            modi.move();
+            
+        }
+    
+        modi.turnLeft();
+        //get the robot to keep moving until it reaches avenue 0
+        while(modi.getAvenue() != 0){
+            modi.move();
+        }
+        
+        
+        
+    }
+}
