8b5b59730dcfc1a226c0adeb71946ea6c0fb0154
modia9548 <modia9548@HRH-IC0027332.wrdsb.ca>
2017-09-25 14:19:30 -0400

    Working on bonus question

--- src/Assignment2/A2Q5.java
+++ src/Assignment2/A2Q5.java
@@ -15,77 +15,110 @@ import becker.robots.Wall;
  * @author Aakash Modi
  */
 public class A2Q5 {
-    
-    
-    
+
     public static void main(String[] args) {
-        
-         //create the city
-     //create the city
-        City aakash=new City();
+
+        //create the city
+        //create the city
+        City aakash = new City();
         //create the robot guard
-        Robot modi=new Robot(aakash,0,0,Direction.SOUTH);
+        Robot modi = new Robot(aakash, 0, 0, Direction.SOUTH);
         //create the driveway
-        new Wall(aakash,0,0,Direction.EAST);
-        new Wall(aakash,1,1,Direction.NORTH);
-        new Wall(aakash,1,2,Direction.NORTH);
-        new Wall(aakash,1,3,Direction.NORTH);
-        new Wall(aakash,1,4,Direction.NORTH);
-        new Wall(aakash,1,4,Direction.EAST);
-        new Wall(aakash,2,4,Direction.EAST);
-        new Wall(aakash,2,4,Direction.SOUTH);
-        new Wall(aakash,2,3,Direction.SOUTH);
-        new Wall(aakash,2,2,Direction.SOUTH);
-        new Wall(aakash,2,1,Direction.SOUTH);
-        new Wall(aakash,3,0,Direction.EAST);
-        new Wall(aakash,4,1,Direction.NORTH);
-        new Wall(aakash,4,2,Direction.NORTH);
-        new Wall(aakash,4,3,Direction.NORTH);
-        new Wall(aakash,4,3,Direction.EAST);
-        new Wall(aakash,4,3,Direction.SOUTH);
-        new Wall(aakash,4,2,Direction.SOUTH);
-        new Wall(aakash,4,1,Direction.SOUTH);
-        new Wall(aakash,5,0,Direction.EAST);
-        new Wall(aakash,6,0,Direction.EAST);
-        new Wall(aakash,7,1,Direction.NORTH);
-        new Wall(aakash,7,2,Direction.NORTH);
-        new Wall(aakash,7,3,Direction.NORTH);
-        new Wall(aakash,7,4,Direction.NORTH);
-        new Wall(aakash,7,5,Direction.NORTH);
-        new Wall(aakash,7,5,Direction.EAST);
-        new Wall(aakash,8,5,Direction.EAST);
-        new Wall(aakash,8,5,Direction.SOUTH);
-        new Wall(aakash,8,4,Direction.SOUTH);
-        new Wall(aakash,8,3,Direction.SOUTH);
-        new Wall(aakash,8,2,Direction.SOUTH);
-        new Wall(aakash,8,1,Direction.SOUTH);
-        new Wall(aakash,9,0,Direction.EAST);
-        new Wall(aakash,9,0,Direction.SOUTH);
+        new Wall(aakash, 0, 0, Direction.EAST);
+        new Wall(aakash, 1, 1, Direction.NORTH);
+        new Wall(aakash, 1, 2, Direction.NORTH);
+        new Wall(aakash, 1, 3, Direction.NORTH);
+        new Wall(aakash, 1, 4, Direction.NORTH);
+        new Wall(aakash, 1, 4, Direction.EAST);
+        new Wall(aakash, 2, 4, Direction.EAST);
+        new Wall(aakash, 2, 4, Direction.SOUTH);
+        new Wall(aakash, 2, 3, Direction.SOUTH);
+        new Wall(aakash, 2, 2, Direction.SOUTH);
+        new Wall(aakash, 2, 1, Direction.SOUTH);
+        new Wall(aakash, 3, 0, Direction.EAST);
+        new Wall(aakash, 4, 1, Direction.NORTH);
+        new Wall(aakash, 4, 2, Direction.NORTH);
+        new Wall(aakash, 4, 3, Direction.NORTH);
+        new Wall(aakash, 4, 3, Direction.EAST);
+        new Wall(aakash, 4, 3, Direction.SOUTH);
+        new Wall(aakash, 4, 2, Direction.SOUTH);
+        new Wall(aakash, 4, 1, Direction.SOUTH);
+        new Wall(aakash, 5, 0, Direction.EAST);
+        new Wall(aakash, 6, 0, Direction.EAST);
+        new Wall(aakash, 7, 1, Direction.NORTH);
+        new Wall(aakash, 7, 2, Direction.NORTH);
+        new Wall(aakash, 7, 3, Direction.NORTH);
+        new Wall(aakash, 7, 4, Direction.NORTH);
+        new Wall(aakash, 7, 5, Direction.NORTH);
+        new Wall(aakash, 7, 5, Direction.EAST);
+        new Wall(aakash, 8, 5, Direction.EAST);
+        new Wall(aakash, 8, 5, Direction.SOUTH);
+        new Wall(aakash, 8, 4, Direction.SOUTH);
+        new Wall(aakash, 8, 3, Direction.SOUTH);
+        new Wall(aakash, 8, 2, Direction.SOUTH);
+        new Wall(aakash, 8, 1, Direction.SOUTH);
+        new Wall(aakash, 9, 0, Direction.EAST);
+        new Wall(aakash, 9, 0, Direction.SOUTH);
         //set up all the items
-        new Thing(aakash,1,0);
-        new Thing(aakash,1,1);
-        new Thing(aakash,1,2);
-        new Thing(aakash,1,3);
-        new Thing(aakash,2,0);
-        new Thing(aakash,2,2);
-        new Thing(aakash,2,4);
-        new Thing(aakash,4,0);
-        new Thing(aakash,4,1);
-        new Thing(aakash,4,2);
-        new Thing(aakash,7,0);
-        new Thing(aakash,7,1);
-        new Thing(aakash,7,5);
-        new Thing(aakash,8,1);
-        new Thing(aakash,8,2);
-        new Thing(aakash,8,4);
+        new Thing(aakash, 1, 0);
+        new Thing(aakash, 1, 1);
+        new Thing(aakash, 1, 2);
+        new Thing(aakash, 1, 3);
+        new Thing(aakash, 2, 0);
+        new Thing(aakash, 2, 2);
+        new Thing(aakash, 2, 4);
+        new Thing(aakash, 4, 0);
+        new Thing(aakash, 4, 1);
+        new Thing(aakash, 4, 2);
+        new Thing(aakash, 7, 0);
+        new Thing(aakash, 7, 1);
+        new Thing(aakash, 7, 5);
+        new Thing(aakash, 8, 1);
+        new Thing(aakash, 8, 2);
+        new Thing(aakash, 8, 4);
         //create algorithm for robot to follow
-        while (modi.leftIsClear()){
+        //move,check for driveway
+        
+        modi.turnLeft();
+        
+            
+        //if driveway
+        while (modi.frontIsClear()) {
+            modi.move();
+        }else{
+                if(!modi.frontIsClear)
+
+                    modi.turnLeft();
+                    modi.turnLeft();
+                    modi.turnLeft();
+                    modi.move();
+    }else{
+            if (modi.canPickThing()) {
+                modi.pickThing();
+            }
+                if (!modi.frontIsClear()) {
+                    modi.turnLeft();
+                }
+                    modi.turnLeft();
+                    modi.turnLeft();
+                    modi.move();
+                
+                    if (!modi.frontIsClear()) {
+                        modi.turnLeft();
+                    }
+
+    
+                    }else{
+                if (!modi.frontIsClear()) {
+            modi.turnLeft();
+
             modi.turnLeft();
         }
-        
-        
     }
-   
-        
-    
 }
+
+
+
+    
+
+
