436db9148ae9a9c78b998789bf9c80008b2c7861
modia9548 <modia9548@HRH-IC0027333.wrdsb.ca>
2017-09-28 13:18:44 -0400

    I have started assignment 3

---
+++ src/Assignment3/A3Q1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author Aakash Modi
+ */
+public class A3Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create city/robot/walls
+        City aakash=new City();
+        Robot modi=new Robot(aakash,0,2, Direction.WEST);
+        new Wall(aakash,1,2,Direction.NORTH);
+        new Wall(aakash,1,1,Direction.NORTH);
+        new Wall(aakash,2,2,Direction.SOUTH);       
+        new Wall(aakash,1,2,Direction.EAST);
+        new Wall(aakash,2,2,Direction.SOUTH);
+        new Wall(aakash,2,2,Direction.EAST);
+        
+    }
+}
