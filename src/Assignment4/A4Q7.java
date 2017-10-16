bbccb3eb9e73ff301c141d9d82bb1ddbc6979e12
modia9548 <modia9548@HRH-IC0027333.wrdsb.ca>
2017-10-16 13:18:43 -0400

    I have begun A4Q5

--- src/Assignment4/A4Q7.java
+++ src/Assignment4/A4Q7.java
@@ -4,6 +4,8 @@
  */
 package Assignment4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aakash Modi
@@ -14,6 +16,14 @@ public class A4Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int win = 1;
+        while (win < 100) {
+            int score = input.nextInt();
+            System.out.println("Choose a number between 1 and 12");
+            score = score;
+        
+        win = score;
+        }
     }
 }
