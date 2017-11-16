15146faa9eb84fd4647bdcdd44a06e55af452d61
modia9548 <modia9548@HRH-IC0027333.wrdsb.ca>
2017-11-16 13:19:58 -0500

    I have completed A5

---
+++ src/Assignment6/A6Q2.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aakash Modi
+ */
+public class A6Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     //create a scanner
+        Scanner input = new Scanner(System.in);
+
+        //ask how many students are in the class
+        System.out.println("How many people are there");
+        int numberOfPeople = input.nextInt();
+        //array to store number of students
+        double[] people = new double[numberOfPeople];
+
+        //create integer to get all of the marks
+        for (int i = 0; i < people.length; i++) {
+
+            System.out.println("What was the height of person " + (i + 1));
+
+            int heightOfPeople = input.nextInt();
+
+            people[i] = heightOfPeople;
+        }
+        double total = 0;
+        for (int a = 0; a < people.length; a++) {
+
+            total = total + people[a];
+        }
+        
+       double answer = Math.round(total*100/numberOfPeople)/100;
+        System.out.println("The average height is " + answer + "cm");
+       for (int i = 0; i < people.length; i++) {
+           if (people[i]>answer){
+               System.out.println(people[i] + "cm are above average height");
+               
+           }
+           
+       }
+           
+    }
+}
---
+++ src/Assignment6/A6Q3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+/**
+ *
+ * @author Aakash Modi
+ */
+public class A6Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        re
+    }
+}
