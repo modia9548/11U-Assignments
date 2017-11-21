bc45bfb9d3e370f9d40cd816c3f6fa01a71b025a
modia9548 <modia9548@HRH-IC0027333.wrdsb.ca>
2017-11-21 14:25:41 -0500

    I have completed A5

--- src/Assignment6/A6Q3.java
+++ src/Assignment6/A6Q3.java
@@ -19,22 +19,20 @@ public class A6Q3 {
 
         Scanner input = new Scanner(System.in);
 
-        //enter integer 1
-        System.out.println("Enter amount of integers ");
-        int integerAmount = input.nextInt();
-
         //array to store integers
-        double[] integer = new double[integerAmount];
-        
-        //create integer to get all of the integers
-        for (int i = 0; i < integer.length; i++) {
-            
-             System.out.println("What is integer " + (i + 1));
-             
-              int orderOfIntegers = input.nextInt();
+        double[] integer = new double[2];
 
-            integer[i] = orderOfIntegers;
-        }
-        double answer = 
+        //enter the integers
+        System.out.println("enter the two integers ");
+        integer[0] = input.nextInt();
+        integer[1] = input.nextInt();
+
+        if (integer[0] > integer[1]) {
+            int.tempNum= integer[0];
+            integer[0]= integer[1];
+            tempNum = integer[1];
+
+
+
     }
 }
---
+++ src/Assignment6/A6Q4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aakash Modi
+ */
+public class A6Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Scanner input = new Scanner(System.in);
+
+        //array to store integers
+        double[] marks = new double[10];
+        for ( int x=0; x<length-1; x++){
+    }
+}
